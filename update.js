ab461ee6398c8891a452e9add61a75d2bb0614be
ishimarutomohisa <luminary95226@sina.com>
2026-02-26 14:37:20 +0800

    Update update.js

--- update.js
+++ update.js
@@ -2,41 +2,40 @@
 
 // 1. 版本配置
 var CONFIG = {
-  version: '102',
+  version: '103',
   // 👇 新增 note 字段：如果有内容则显示，为空则不显示
-  note: `⚠️ Dreamsparks will not be available in UP101, most probably in UP103.
+  note: `
 `
   // note: '' // 如果想隐藏，就留空
 };
 
 // 2. 特殊日期标线配置 (新增部分)
 var specialDates = [
-  { date: '2025-12-25', label: '🎁', color: '#30C2FF' }, // 圣诞节
   // 你可以随时添加新的，例如：
   // { date: '2026-01-01', label: '🎉', color: '#FFD700' }, 
 ];
 
 // 3. 甘特图排期数据
 var schedules = [
-  { resource: '', tasks: [{start: '2025-12-10', end: '2025-12-14', label: 'Virus'}] },
-  { resource: '', tasks: [{start: '2025-12-11', end: '2025-12-14', label: 'Storm Cloud*'}] },
-  { resource: '', tasks: [{start: '2025-12-24', end: '2025-12-29', label: 'Dreamland Quest'}] },
+  { resource: '', tasks: [{start: '2026-02-06', end: '2026-02-21', label: 'Story Pass'}] },
+  { resource: '', tasks: [{start: '2026-02-05', end: '2026-02-11', label: 'Cascading Tapper Event'}] },
+  { resource: '', tasks: [{start: '2026-03-05', end: '2026-03-08', label: 'Dandelion Fluff*'}] },
+  { resource: '', tasks: [{start: '2026-03-01', end: '2026-03-05', label: 'Cursed Crab'}] },
+  { resource: '', tasks: [{start: '2026-03-02', end: '2026-03-07', label: 'Dreamland Quest'}] },
   { resource: '', tasks: [
-      {start: '2025-12-15', end: '2026-01-05', label: 'Avatar – Mega Event'},
-      {start: '2025-12-15', end: '2025-12-18', label: 'Chapter 1'},
-      {start: '2025-12-18', end: '2025-12-24', label: 'Chapter 2'},
-      {start: '2025-12-24', end: '2025-12-31', label: 'Chapter 3'},
-      {start: '2025-12-31', end: '2026-01-05', label: 'Chapter 4'},
-      {start: '2025-12-18', end: '2025-12-21', label: 'Striking Gold'},
-      {start: '2025-12-25', end: '2025-12-28', label: 'Striking Gold'},
-      {start: '2026-01-01', end: '2026-01-04', label: 'Striking Gold'},
+      {start: '2026-02-12', end: '2026-02-27', label: 'Disney Fairies Mini Event'},
+      {start: '2026-02-12', end: '2026-02-14', label: 'Chapter 1'},
+      {start: '2026-02-14', end: '2026-02-21', label: 'Chapter 2'},
+      {start: '2026-02-21', end: '2026-02-27', label: 'Chapter 3'},
+      {start: '2026-02-13', end: '2026-02-16', label: 'Striking Gold'},
+      {start: '2026-02-18', end: '2026-02-21', label: 'Striking Gold'},
+      {start: '2026-02-23', end: '2026-02-26', label: 'Striking Gold'},
     ]
   }, 
 ];
 
 // 2. 特殊日期标线配置 (新增部分)
 var specialDates2 = [
-  { date: '2026-01-06', label: '🤰', color: '#30C2FF' }, // 圣诞节
   // 你可以随时添加新的，例如：
   // { date: '2026-01-01', label: '🎉', color: '#FFD700' }, 
 ];
@@ -54,3 +53,4 @@ var schedules2 = [
   { resource: '', tasks: [{start: '2026-01-27', end: '2026-02-01', label: 'Battle Bot'}] },
   { resource: '', tasks: [{start: '2026-01-25', end: '2026-01-30', label: 'Dreamland Quest'}] },
 ];
+
